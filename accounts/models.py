eb8354bb58ebfeaf2153a10c59829064332f9fab
Mohammadreza-Mos-hafian <mreza.moshafian@gmail.com>
2026-09-07 14:33:35 +0330

    fix migrations

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models    
 
-class User(AbstractUser):
+class User(AbstractUser, PermissionsMixin):
     phone_number = models.CharField(max_length=15, unique=True, blank=True, null=True)
     is_golden = models.BooleanField(default=False, verbose_name='اشتراک طلایی')
     golden_expiry = models.DateTimeField(blank=True, null=True, verbose_name='تاریخ انقضای طلایی')
-    
+
     class Meta:
         verbose_name = 'کاربر'
         verbose_name_plural = 'کاربران'
