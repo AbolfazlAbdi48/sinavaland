93878757234d56806ca7c9a9a4927ce6856f3ecf
Mohammadreza-Mos-hafian <mreza.moshafian@gmail.com>
2026-09-14 14:11:33 +0330

    modify otp service

--- accounts/otp_service.py
+++ accounts/otp_service.py
@@ -9,26 +9,15 @@ from datetime import timedelta
 
 def send_otp(user):
     code = f"{secrets.randbelow(1_000_000):06d}"
-
-    print("STEP 1 - CODE:", code)
-    print("STEP 2 - USERNAME:", repr(user.username))
-
     success = send_otp_sms(user.username, code)
 
-    print("STEP 3 - SMS SUCCESS:", success)
-
     if not success:
-        print("STEP 3.5 - SMS SERVICE RETURNED FALSE")
         return False
 
-    print("STEP 4 - CREATING OTP")
-
-    otp = OTP.objects.create(
+    OTP.objects.create(
         user=user,
         code=code,
         expires_at=timezone.now() + timedelta(minutes=2)
     )
-
-    print("STEP 5 - OTP CREATED:", otp.id)
 
     return True
