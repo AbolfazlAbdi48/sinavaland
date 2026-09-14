fad3db27a6c880a64247139ed6a80e6f0b01ebde
Mohammadreza-Mos-hafian <mreza.moshafian@gmail.com>
2026-09-14 14:04:20 +0330

    modify otp service

--- accounts/otp_service.py
+++ accounts/otp_service.py
@@ -8,30 +8,27 @@ from datetime import timedelta
 
 
 def send_otp(user):
-    try:
-        code = f"{secrets.randbelow(1_000_000):06d}"
+    code = f"{secrets.randbelow(1_000_000):06d}"
 
-        print("STEP 1 - CODE:", code)
-        print("STEP 2 - USERNAME:", repr(user.username))
+    print("STEP 1 - CODE:", code)
+    print("STEP 2 - USERNAME:", repr(user.username))
 
-        success = send_otp_sms(user.username, code)
+    success = send_otp_sms(user.username, code)
 
-        print("STEP 3 - SMS SUCCESS:", success)
+    print("STEP 3 - SMS SUCCESS:", success)
 
-        if not success:
-            return False
-
-        otp = OTP.objects.create(
-            user=user,
-            code=code,
-            expires_at=timezone.now() + timedelta(minutes=2)
-        )
-
-        print("STEP 4 - OTP CREATED:", otp.id)
-
-        return True
-
-    except Exception as e:
-        print("SEND OTP ERROR TYPE:", type(e).__name__)
-        print("SEND OTP ERROR:", repr(e))
+    if not success:
+        print("STEP 3.5 - SMS SERVICE RETURNED FALSE")
         return False
+
+    print("STEP 4 - CREATING OTP")
+
+    otp = OTP.objects.create(
+        user=user,
+        code=code,
+        expires_at=timezone.now() + timedelta(minutes=2)
+    )
+
+    print("STEP 5 - OTP CREATED:", otp.id)
+
+    return True
