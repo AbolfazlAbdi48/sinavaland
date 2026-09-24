84849e6a67ecd18fa646223b7cdc0dcbb4e31fec
Mohammadreza-Mos-hafian <mreza.moshafian@gmail.com>
2026-09-24 23:01:07 +0330

    Change estate module ui

--- estate/admin.py
+++ estate/admin.py
@@ -1,19 +1,29 @@
 from django.contrib import admin
-from .models import Estate
+from .models import Estate, EstateImage
+
 
 @admin.register(Estate)
 class EstateAdmin(admin.ModelAdmin):
-    list_display = ['title', 'destination','get_reservation_count', 'duration', 'price', 'start_date', 'capacity', 'is_available']
+    list_display = ['title', 'destination', 'get_reservation_count', 'duration', 'price', 'start_date', 'capacity',
+                    'is_available']
     list_filter = ['duration', 'is_available', 'start_date']
-    search_fields = ['title','get_reservation_count', 'destination', 'description']
+    search_fields = ['title', 'get_reservation_count', 'destination', 'description']
     prepopulated_fields = {'slug': ('title',)}
     list_editable = ['is_available']
     date_hierarchy = 'start_date'
     filter_horizontal = ['reserved_by']
 
     def get_reservation_count(self, obj):
-            return obj.reserved_by.count()
+        return obj.reserved_by.count()
+
     get_reservation_count.short_description = 'تعداد رزرو'
+
 
 admin.site.unregister(Estate)  # If already registered
 admin.site.register(Estate, EstateAdmin)
+
+
+@admin.register(EstateImage)
+class EstateImageAdmin(admin.ModelAdmin):
+    list_display = ['estate', 'image', 'created_at']
+    list_filter = ['created_at']
--- estate/models.py
+++ estate/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils.text import slugify
 from django.conf import settings
 
+from accounts.models import phone_validator
+
+
 class Estate(models.Model):
     DURATION_CHOICES = [
         ('1day', 'یک روزه'),
@@ -10,28 +13,29 @@ class Estate(models.Model):
         ('week', 'یک هفته'),
     ]
 
-    title           = models.CharField(max_length=200, verbose_name='عنوان')
-    slug            = models.SlugField(unique=True, allow_unicode=True)
-    destination     = models.CharField(max_length=200, verbose_name='مقصد')
-    duration        = models.CharField(max_length=20, choices=DURATION_CHOICES, verbose_name='مدت')
-    description     = models.TextField(verbose_name='توضیحات')
-    itinerary       = models.TextField(blank=True, verbose_name='برنامه سفر')
-    included_services  = models.TextField(blank=True, verbose_name='خدمات شامل شده')
-    excluded_services  = models.TextField(blank=True, verbose_name='خدمات شامل نشده')
-    price           = models.DecimalField(max_digits=10, decimal_places=0, verbose_name='قیمت')
-    capacity        = models.PositiveIntegerField(verbose_name='ظرفیت')
-    start_date      = models.DateField(verbose_name='تاریخ شروع')
-    end_date        = models.DateField(verbose_name='تاریخ پایان', blank=True, null=True)
-    image           = models.ImageField(upload_to='estate/', blank=True, null=True)
-    is_available    = models.BooleanField(default=True, verbose_name='موجود')
-    reserved_by     = models.ManyToManyField(
+    title = models.CharField(max_length=200, verbose_name='عنوان')
+    slug = models.SlugField(unique=True, allow_unicode=True)
+    destination = models.CharField(max_length=200, verbose_name='مقصد')
+    duration = models.CharField(max_length=20, choices=DURATION_CHOICES, verbose_name='مدت')
+    description = models.TextField(verbose_name='توضیحات')
+    itinerary = models.TextField(blank=True, verbose_name='برنامه سفر')
+    included_services = models.TextField(blank=True, verbose_name='خدمات شامل شده')
+    excluded_services = models.TextField(blank=True, verbose_name='خدمات شامل نشده')
+    price = models.DecimalField(max_digits=10, decimal_places=0, verbose_name='قیمت')
+    capacity = models.PositiveIntegerField(verbose_name='ظرفیت')
+    start_date = models.DateField(verbose_name='تاریخ شروع')
+    end_date = models.DateField(verbose_name='تاریخ پایان', blank=True, null=True)
+    is_available = models.BooleanField(default=True, verbose_name='موجود')
+    owner_phone_number = models.CharField(max_length=11, blank=True, null=True, verbose_name='شماره تماس مالک',
+                                          validators=[phone_validator])
+    reserved_by = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
         blank=True,
         related_name='reserved_estate',
         verbose_name='رزرو شده توسط'
     )
-    created_at  = models.DateTimeField(auto_now_add=True)
-    updated_at  = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = 'ملک'
@@ -49,3 +53,20 @@ class Estate(models.Model):
     @property
     def available_seats(self):
         return self.capacity - self.reserved_by.count()
+
+
+class EstateImage(models.Model):
+    estate = models.ForeignKey(
+        Estate,
+        on_delete=models.CASCADE,
+        related_name='images'
+    )
+
+    image = models.ImageField(upload_to='estate/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'تصویر ملک'
+        verbose_name_plural = 'تصاویر املاک'
+        ordering = ['created_at']
