13bf839b158e739426b50b0db935d91d7c20dd38
Mohammadreza-Mos-hafian <mreza.moshafian@gmail.com>
2026-09-25 20:48:05 +0330

    fix url

--- estate/urls.py
+++ estate/urls.py
@@ -6,8 +6,8 @@ app_name = 'estate'
 urlpatterns = [
     # fixed paths must come BEFORE the <persian_slug:slug>/ catch-all
     path('my/reservations/', views.my_reservations, name='my_reservations'),
-    path('', views.estate_list, name='list'),
     path('<persian_slug:slug>/', views.estate_detail, name='detail'),
+    path('', views.estate_list, name='list'),
     path('<persian_slug:slug>/reserve/', views.reserve_estate, name='reserve'),
     path('<persian_slug:slug>/cancel/', views.cancel_reservation, name='cancel_reservation'),
 ]
